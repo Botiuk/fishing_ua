49b26af06be80d863d21c4bce0ca13db64ed8100
botiuk <svetabotiuk@gmail.com>
2024-05-06 14:55:47 +0300

    changed header, js config

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap-sprockets", to: "bootstrap-sprockets.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "popper", to: "popper.js", preload: true
+pin "activestorage", to: "activestorage.js"
