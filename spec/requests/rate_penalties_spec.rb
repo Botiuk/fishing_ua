8f0b7a3b1aa0caeb9a38619dbdf5da02cce0db14
botiuk <svetabotiuk@gmail.com>
2024-05-21 10:30:31 +0300

    add requests tests for water_bioresources and rate_penalties with abilities for users roles

--- spec/requests/rate_penalties_spec.rb
+++ spec/requests/rate_penalties_spec.rb
@@ -1,47 +1,143 @@
 require 'rails_helper'
 
 RSpec.describe "RatePenalties", type: :request do
-  before :each do
-    user = create(:user, role: "admin")
-    login_as(user, :scope => :user)
+  describe "non registered user management" do
+    it "can GET index" do
+      get rate_penalties_path
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
+
+    it "cannot GET new and redirects to the sign_in page" do
+      get new_rate_penalty_path
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
+
+    it "cannot GET edit and redirects to the sign_in page" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
+
+    it "cannot DELETE destroy and redirects to the sign_in page" do
+      rate_penalty = create(:rate_penalty)
+      delete rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
   end
 
-  it "GET index" do
-    get rate_penalties_path
-    expect(response).to be_successful
+  describe "user-user management" do
+    before :each do
+      user = create(:user)
+      login_as(user, :scope => :user)
+    end
+
+    it "can GET index" do
+      get rate_penalties_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET new and redirects to the root page" do
+      get new_rate_penalty_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET edit and redirects to the root page" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot DELETE destroy and redirects to root page" do
+      rate_penalty = create(:rate_penalty)
+      delete rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
   end
 
-  it "GET new" do
-    get new_rate_penalty_path
-    expect(response).to be_successful
+  describe "user-staff management" do
+    before :each do
+      user = create(:user, role: "staff")
+      login_as(user, :scope => :user)
+    end
+
+    it "can GET index" do
+      get rate_penalties_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET new and redirects to the root page" do
+      get new_rate_penalty_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET edit and redirects to the root page" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot DELETE destroy and redirects to root page" do
+      rate_penalty = create(:rate_penalty)
+      delete rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
   end
 
-  it "POST create" do
-    water_bioresource = create(:water_bioresource)
-    post rate_penalties_path, params: { rate_penalty: attributes_for(:rate_penalty, water_bioresource_id: water_bioresource.id) }
-    expect(response).to be_redirect
-    follow_redirect!
-    expect(flash[:notice]).to include(I18n.t('notice.create.rate_penalty'))
-  end
+  describe "user-admin management" do
+    before :each do
+      user = create(:user, role: "admin")
+      login_as(user, :scope => :user)
+    end
 
-  it "GET edit" do
-    rate_penalty = create(:rate_penalty)
-    get edit_rate_penalty_path(rate_penalty)
-    expect(response).to be_successful
-  end
+    it "GET index" do
+      get rate_penalties_path
+      expect(response).to be_successful
+    end
 
-  it "PUT update" do
-    rate_penalty = create(:rate_penalty, money: 12345)
-    put rate_penalty_path(rate_penalty), params: { rate_penalty: {money: 54321} }
-    expect(rate_penalty.reload.money).to eq(54321)
-    expect(response).to redirect_to(rate_penalties_url)
-    expect(flash[:notice]).to include(I18n.t('notice.update.rate_penalty'))
-  end
+    it "GET new" do
+      get new_rate_penalty_path
+      expect(response).to be_successful
+    end
 
-  it "DELETE destroy" do
-    rate_penalty = create(:rate_penalty)
-    delete rate_penalty_path(rate_penalty)
-    expect(response).to redirect_to(rate_penalties_url)
-    expect(flash[:notice]).to include(I18n.t('notice.destroy.rate_penalty'))
+    it "POST create" do
+      water_bioresource = create(:water_bioresource)
+      post rate_penalties_path, params: { rate_penalty: attributes_for(:rate_penalty, water_bioresource_id: water_bioresource.id) }
+      expect(response).to be_redirect
+      follow_redirect!
+      expect(flash[:notice]).to include(I18n.t('notice.create.rate_penalty'))
+    end
+
+    it "GET edit" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to be_successful
+    end
+
+    it "PUT update" do
+      rate_penalty = create(:rate_penalty, money: 12345)
+      put rate_penalty_path(rate_penalty), params: { rate_penalty: {money: 54321} }
+      expect(rate_penalty.reload.money).to eq(54321)
+      expect(response).to redirect_to(rate_penalties_url)
+      expect(flash[:notice]).to include(I18n.t('notice.update.rate_penalty'))
+    end
+
+    it "DELETE destroy" do
+      rate_penalty = create(:rate_penalty)
+      delete rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(rate_penalties_url)
+      expect(flash[:notice]).to include(I18n.t('notice.destroy.rate_penalty'))
+    end
   end
 end
--- spec/requests/water_bioresources_spec.rb
+++ spec/requests/water_bioresources_spec.rb
@@ -1,39 +1,114 @@
 require 'rails_helper'
 
 RSpec.describe "WaterBioresources", type: :request do
-  before :each do
-    user = create(:user, role: "admin")
-    login_as(user, :scope => :user)
+  describe "non registered user management" do
+    it "can GET index" do
+      get water_bioresources_path
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
+
+    it "cannot GET new and redirects to the sign_in page" do
+      get new_water_bioresource_path
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
+
+    it "cannot GET edit and redirects to the sign_in page" do
+      water_bioresource = create(:water_bioresource)
+      get edit_water_bioresource_path(water_bioresource)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to include I18n.t('devise.failure.unauthenticated')
+    end
   end
 
-  it "GET index" do
-    get water_bioresources_path
-    expect(response).to be_successful
+  describe "user-user management" do
+    before :each do
+      user = create(:user)
+      login_as(user, :scope => :user)
+    end
+
+    it "can GET index" do
+      get rate_penalties_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET new and redirects to the root page" do
+      get new_rate_penalty_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET edit and redirects to the root page" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
   end
 
-  it "GET new" do
-    get new_water_bioresource_path
-    expect(response).to be_successful
+  describe "user-staff management" do
+    before :each do
+      user = create(:user, role: "staff")
+      login_as(user, :scope => :user)
+    end
+
+    it "can GET index" do
+      get rate_penalties_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET new and redirects to the root page" do
+      get new_rate_penalty_path
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
+
+    it "cannot GET edit and redirects to the root page" do
+      rate_penalty = create(:rate_penalty)
+      get edit_rate_penalty_path(rate_penalty)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to include I18n.t('alert.access_denied')
+    end
   end
 
-  it "POST create" do
-    post water_bioresources_path, params: { water_bioresource: attributes_for(:water_bioresource) }
-    expect(response).to be_redirect
-    follow_redirect!
-    expect(flash[:notice]).to include(I18n.t('notice.create.water_bioresource'))
-  end
+  describe "user-admin management" do
+    before :each do
+      user = create(:user, role: "admin")
+      login_as(user, :scope => :user)
+    end
 
-  it "GET edit" do
-    water_bioresource = create(:water_bioresource)
-    get edit_water_bioresource_path(water_bioresource)
-    expect(response).to be_successful
-  end
+    it "GET index" do
+      get water_bioresources_path
+      expect(response).to be_successful
+    end
 
-  it "PUT update" do
-    water_bioresource = create(:water_bioresource, name: "Abcd")
-    put water_bioresource_path(water_bioresource), params: { water_bioresource: {name: "Dbca"} }
-    expect(water_bioresource.reload.name).to eq("Dbca")
-    expect(response).to redirect_to(water_bioresources_url)
-    expect(flash[:notice]).to include(I18n.t('notice.update.water_bioresource'))
+    it "GET new" do
+      get new_water_bioresource_path
+      expect(response).to be_successful
+    end
+
+    it "POST create" do
+      post water_bioresources_path, params: { water_bioresource: attributes_for(:water_bioresource) }
+      expect(response).to be_redirect
+      follow_redirect!
+      expect(flash[:notice]).to include(I18n.t('notice.create.water_bioresource'))
+    end
+
+    it "GET edit" do
+      water_bioresource = create(:water_bioresource)
+      get edit_water_bioresource_path(water_bioresource)
+      expect(response).to be_successful
+    end
+
+    it "PUT update" do
+      water_bioresource = create(:water_bioresource, name: "Abcd")
+      put water_bioresource_path(water_bioresource), params: { water_bioresource: {name: "Dbca"} }
+      expect(water_bioresource.reload.name).to eq("Dbca")
+      expect(response).to redirect_to(water_bioresources_url)
+      expect(flash[:notice]).to include(I18n.t('notice.update.water_bioresource'))
+    end
   end
 end
