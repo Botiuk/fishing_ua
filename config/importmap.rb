dc72fc940a7fc4577e1fe7a8f80e0f2659509c3c
botiuk <svetabotiuk@gmail.com>
2024-05-29 12:55:03 +0300

    add Action Text

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,4 +10,6 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "jquery", to: "jquery.min.js", preload: true
 pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin "popper", to: "popper.js", preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
 pin "activestorage", to: "activestorage.js"
