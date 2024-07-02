720be7ffb4fb8d35db1a1aa92d0cae8b0c2659ea
botiuk <svetabotiuk@gmail.com>
2024-07-02 12:23:05 +0300

    add new content to profile statistic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,8 +6,13 @@ class MainController < ApplicationController
     end
 
     def statistic
+        @fishing_sessions_count = FishingSession.where(user_id: current_user.id).count
+        @fishing_sessions_duration = FishingSession.duration_all_sessions(current_user.id) if @fishing_sessions_count != 0
+        @fishing_places_count = FishingPlace.where(user_id: current_user.id).count
+        @equipments_count = Tool.where(user_id: current_user.id, tool_type: "equipment").count
+        @baits_count = Tool.where(user_id: current_user.id, tool_type: "bait").count
         @catches_count = Catch.statistic_all_records(current_user.id, "user", current_user.id)
-        if @catches_count.present?
+        if @catches_count != 0
             @catches_max_weight = Catch.statistic_max_weight(current_user.id, "user", current_user.id)
             @catches_max_length = Catch.statistic_max_length(current_user.id, "user", current_user.id)
             @water_bioresources = WaterBioresource.statistic_all_records(current_user.id, "user", current_user.id)
--- app/models/fishing_session.rb
+++ app/models/fishing_session.rb
@@ -5,4 +5,19 @@ class FishingSession < ApplicationRecord
 
   validates :start_at, presence: true
   validates :end_at, comparison: { greater_than: :start_at }, allow_blank: true
+
+  private
+
+  def self.duration_all_sessions(user_id)
+    fishing_sessions = FishingSession.where(user_id: user_id)
+    duration_seconds = 0
+    fishing_sessions.each do |fishing_session|
+      fishing_session.end_at = DateTime.now if fishing_session.end_at == nil
+      duration_seconds += (fishing_session.end_at - fishing_session.start_at).abs
+    end
+    duration_days = (duration_seconds / 86400).to_i
+    duration_day_seconds = (duration_seconds % 86400)
+    duration_time = Time.at(duration_day_seconds).utc.strftime "%H:%M"    
+    "#{duration_days} #{I18n.t('statistic.duration_days')} #{duration_time}"
+  end
 end
